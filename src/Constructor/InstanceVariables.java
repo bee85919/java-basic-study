f7fed8a5ec2a0e48f277f2182c64be7b8055c2c7
bee85919 <bee85919@gmail.com>
2023-08-21 16:29:10 +0900

    Fix ConstructorExample.java Add InstanceVariables.java

---
+++ src/Constructor/InstanceVariables.java
@@ -0,0 +1,26 @@
+package Constructor;
+
+public class InstanceVariables {
+
+    public static class DefaultValueTest { // static 키워드 추가
+        byte byteDefaultValue;
+        int intDefaultValue;
+        short shortDefaultValue;
+        long longDefaultValue;
+        float floatDefaultValue;
+        double doubleDefaultValue;
+        boolean booleanDefaultValue;
+        String referenceDefaultValue;
+    }
+    public static void main(String[] args) {
+        DefaultValueTest defaultValueTest = new DefaultValueTest();
+        System.out.println("byte default: " + defaultValueTest.byteDefaultValue);
+        System.out.println("short default: " + defaultValueTest.shortDefaultValue);
+        System.out.println("int default: " + defaultValueTest.intDefaultValue);
+        System.out.println("long default: " + defaultValueTest.longDefaultValue);
+        System.out.println("float default: " + defaultValueTest.floatDefaultValue);
+        System.out.println("double default: " + defaultValueTest.doubleDefaultValue);
+        System.out.println("boolean default: " + defaultValueTest.booleanDefaultValue);
+        System.out.println("reference default: " + defaultValueTest.referenceDefaultValue);
+    }
+}
