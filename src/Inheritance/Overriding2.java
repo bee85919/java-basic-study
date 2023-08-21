d4ad2cf4393aee3bbe374becf1a6a7168c24525d
bee85919 <bee85919@gmail.com>
2023-08-21 16:44:32 +0900

    Overriding

---
+++ src/Inheritance/Overriding2.java
@@ -0,0 +1,26 @@
+package Inheritance;
+public class Overriding2 {
+    static class Animal {
+        String name;
+        Animal(String name) {
+            this.name = name;
+        }
+        public void cry() {
+            System.out.println(name + " is crying.");
+        }
+    }
+    static class Giraffe extends Animal {
+        Giraffe(String name) {
+            super(name);
+        }
+        @Override
+        public void cry() {
+            super.cry();
+            System.out.println(name + " cannot cry.");
+        }
+    }
+    public static void main(String[] args) {
+        Animal giraffe = new Giraffe("기린이");
+        giraffe.cry();
+    }
+}
